defb5d1a611083ae486ec9092c4a6ccbb6771fe8
Austin Seto <austinseto@gmail.com>
2017-07-21 18:23:33 -0400

    Edited to have 3 channels
    Also added a song

--- components/song_loader.v
+++ components/song_loader.v
@@ -1,15 +1,26 @@
 `timescale 1ns/1ns
 
-module song_loader(song_select, song_output);
+module song_loader(song_select, output_red, output_blue, output_yellow);
   input [4:0] song_select;
-  output [99:0] song_output;
+  output [99:0] output_red, output_blue, output_yellow;
 
-  reg [99:0] song;
-  assign song_output = song;
+  localparam Through_The_Fire_and_Flames = 4'b1111;// Songs available
+  
+  reg [99:0] red, blue, yellow;
+  assign output_red = red;
+  assign output_blue = blue;
+  assign output_yellow = yellow;
+  
   always @(*)
   begin
     case (song_select[4:0])
       // More cases to be added in future, each case is its own song
+      case Through_The_Fire_and_Flames:
+        begin
+          red    <= 100'b0000000000101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010;
+          yellow <= 100'b0000000000010001000100010001000100010001000100010001000100010001000100010001000100010001000100010001;
+          blue   <= 100'b0000000000000100010001000100010001000100010001000100010001000100010001000100010001000100010001000100;
+        end
       default: song <= 100'b0; // Default is no song
     endcase
   end
