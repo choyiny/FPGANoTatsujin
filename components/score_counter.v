01fe9489c94ad1f1b05ceeaeea691403cb7d0f37
hkminegod <me@techmastary.net>
2017-07-21 17:57:23 -0400

    change how scoring works

--- components/score_counter.v
+++ components/score_counter.v
@@ -19,7 +19,7 @@ module score_counter(
       storage <= 0;
     else if (increase_score)
       storage <= storage + 1;
-    else if (decrease_score)
+    else if (decrease_score && storage != 8'b0)
       storage <= storage - 1;
   end
 endmodule
